eab4364ab48d3a0f81debcf0d212089e86d1f02c
aurapitaloka <aurapita131@gmail.com>
2023-03-22 07:57:32 +0700

    TM01

---
+++ app/src/main/java/com/example/tm1_22090026_aurapitaloka_2b/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tm1_22090026_aurapitaloka_2b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
